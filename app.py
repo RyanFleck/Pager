e78944e21d57d611f95764e394164c4d38a50017
Ryan Fleck <Ryan.Fleck@protonmail.com>
2023-05-07 20:28:16 -0600

    Add basics.

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
+    app.add_url_rule(
+        "/favicon.ico", redirect_to=url_for("static", filename="favicon.ico")
+    )
